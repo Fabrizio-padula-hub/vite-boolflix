fdcdf432d644cb6a400635954f4b15363a1f11b2
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-04-17 16:08:39 +0200

    feat: addition of the global state structure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default{
+  components: {
+    AppHeader,
+  }
+}
 </script>
 
 <template>
-  <h1>Prova</h1>
+  <AppHeader></AppHeader>
 </template>
 
 <style lang="scss">
+@use './style/generic';
+
+
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+    name: 'AppHeader',
+}
+</script>
+
+<template>
+
+    <header>
+        <h1>Header</h1>
+    </header>
+    
+</template>
+
+<style scoped lang="scss">
+
+
+
+</style>
